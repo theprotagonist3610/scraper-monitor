5f9c256f76044919d4f3a63e35a155515225b541
theProtagonist <trouveur3610@gmail.com>
2024-05-19 11:23:41 +0200

    Initialisation du moniteur de scraping

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-9xl font-display">React</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
